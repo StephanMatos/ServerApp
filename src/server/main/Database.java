6aebd067d6938d46ec8213eeb355d253ac0de1d2
Mwthorn <mwthorn@gmail.com>
2016-06-09 16:16:46 +0200

    holder lige tilbage med oprettelse af filer

--- src/server/main/Database.java
+++ src/server/main/Database.java
@@ -46,7 +46,7 @@ public class Database {
         }
         User user = new User(username,password);
         users.put(username,user);
-        FileReader.createUserFile(user);
+        //FileReader.createUserFile(user);
         System.out.println("user created: " + username + " " + password);
         return true;
     }
